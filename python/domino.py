67b39d8cc7b308e8d30b4c8115d542e7104c2da4
한준 <s22073@gsm.hs.kr>
2023-05-01 08:40:40 +0900

    🤙:: 피자 메뉴들 담는 json 파일 생성

--- python/domino.py
+++ python/domino.py
@@ -23,7 +23,7 @@ for tag in menu_list:
 
 
 
-with open('dominos.json', 'w', encoding='utf-8') as f:
+with open('json/dominos.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent='\t')
 
 print('저장완료!')
