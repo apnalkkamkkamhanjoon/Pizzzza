2cce5408431c80ab440cd6bf3badde1f1ceadbc6
한준 <s22073@gsm.hs.kr>
2023-05-01 08:38:02 +0900

    🤙:: domino 메뉴 크롤링

---
+++ python/domino.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+url = 'https://web.dominos.co.kr/goods/list'
+
+# requests 라이브러리를 사용하여 HTML 소스코드를 가져옴
+response = requests.get(url)
+html = response.text
+
+# HTML 소스코드를 파싱하여 BeautifulSoup 객체를 생성
+soup = BeautifulSoup(html, 'html.parser')
+
+data = []
+menu_list = soup.select('div[class="menu_list"]')
+for tag in menu_list:
+    item_name = menu_list.select_one('div[class="subject"]').text.strip()
+    item_price1 = menu_list.select_one('div[class="price-box"]').text.strip()
+
+    # data 리스트에 품명과 가격을 추가
+    data.append({'품명': item_name, '가격L': item_price1})
+
+
+
+
+with open('dominos.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent='\t')
+
+print('저장완료!')
