3a60910ad0e3caf789e6afd00f5e019b8dff7e6a
한준 <s22073@gsm.hs.kr>
2023-05-01 23:51:00 +0900

    📢:: 컴포넌트 수정
    Main -> 날짜 추가
    Pizzacontainer -> state 변수 이름 변경

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,22 @@
 import Pizzacontainer from '../components/Pizzacontainer'
 
+let today = new Date();
+let year = today.getFullYear(); // 년도
+let month = today.getMonth() + 1; // 월
+let date = today.getDate(); // 날짜
+let week = ["일", "월", "화", "수", "목", "금", "토"];
+let dayName = week[today.getDay()];
 
 const Main = () => {
 
   return (
     <>
-      <Pizzacontainer />
+      <div>
+        <p>
+          {year}년 {month}월 {date}일 {dayName}요일
+        </p>
+        <Pizzacontainer />
+      </div>
     </>
   );
 };
