76b020763c92b7fc8d0c6c89a4ffb215f201e698
한준 <s22073@gsm.hs.kr>
2023-05-01 22:37:03 +0900

    📢:: json 잘 가져와지는지 테스트

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,30 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import PaPaJohns from '../json/papajohns.json';
+
+interface Pizza {
+  이미지: string;
+  품명: string;
+}
 
 const Main = () => {
+  const [data, setData] = useState<Pizza[]>([]);
+
+  useEffect(() => {
+    setData(PaPaJohns)
+  }, [])
+
 
   return (
     <>
-      <h1>Pizza</h1>
+      {data.map((item) => (
+        <div key={item.이미지}>
+          <img src={item.이미지} alt={item.품명} />
+          <p>{item.품명}</p>
+        </div>
+      ))}
     </>
-  )
+  );
 };
 
 export default Main;
