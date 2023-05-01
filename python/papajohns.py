f33177575278d0e8f885bada585101b9c83d903f
한준 <s22073@gsm.hs.kr>
2023-05-01 20:15:58 +0900

    🤙:: papajohns 로직 변경

--- python/papajohns.py
+++ python/papajohns.py
@@ -1,26 +1,22 @@
 import requests
-import json
 from bs4 import BeautifulSoup
+import json
 
-# HTML 파일을 가져옴
-with open('public/papajohns.html', encoding='utf-8') as f:
-    html_doc = f.read()
+url = 'https://pji.co.kr/menu/w_productPizza'
 
-# HTML 파싱
-soup = BeautifulSoup(html_doc, 'html.parser')
+response = requests.get(url)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
 
-# 크롤링할 태그 선택
-th = soup.find_all('th')
-td = soup.find_all('td')
-
-# JSON으로 변환
 data = []
-for i in range(len(td) // len(th)):
-    item = {}
-    for j, tag in enumerate(th):
-        item[tag.text.strip()] = td[i * len(th) + j].text.strip()
-    data.append(item)
 
-# JSON 파일로 저장
+for tag in soup.select('div.thumb_wrap > div.thumb > button > img'):
+    data.append({'이미지': tag['src']})
+
+for i, tag in enumerate(soup.select('div.content > div.name')):
+    data[i]['품명'] = tag.text.strip()
+
 with open('json/papajohns.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent='\t')
+
+print('저장완료!')
