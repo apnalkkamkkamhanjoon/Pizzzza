e5dd45216959d84ef41554a64fb0435c245b131b
한준 <s22073@gsm.hs.kr>
2023-05-01 20:39:35 +0900

    🤙:: Mr.pizza 크롤링 로직 구현 시도

---
+++ python/mrpizza.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = 'https://pji.co.kr/menu/w_productPizza'
+
+response = requests.get(url)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+
+data = []
+
+for tag in soup.select('a > img'):
+    data.append({'이미지': tag['src']})
+
+for i, tag in enumerate(soup.select('a > strong')):
+    data[i]['품명'] = tag.text.strip()
+
+with open('json/papajohns.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent='\t')
+
+print('저장완료!')
