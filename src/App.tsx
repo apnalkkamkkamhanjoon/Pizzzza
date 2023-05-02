3efcded258771640df730e868b1f9559cc26018f
한준 <s22073@gsm.hs.kr>
2023-05-02 14:20:32 +0900

    📢:: 페이지 생성 및 라우팅 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import Main from './pages/Main';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Routes } from 'react-router';
+import Domino from './pages/Domino';
+import Papajohns from './components/Pizzacontainer';
+import Mrpizza from './pages/Mrpizza';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Main />}/>
-        <Route path='/domino' element={<Main />}/>
-        <Route path='/papajohns' element={<Main />}/>
-        <Route path='/mrpizza' element={<Main />}/>
+        <Route path='/' element={<Domino />}/>
+        <Route path='/papajohns' element={<Papajohns />}/>
+        <Route path='/mrpizza' element={<Mrpizza />}/>
       </Routes>
     </Router>
   );
---
+++ src/pages/Papajohns.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+const Papajohns = () => {
+    return (
+        <div>
+            <NavBar />
+        </div>
+    );
+};
+
+export default Papajohns;
