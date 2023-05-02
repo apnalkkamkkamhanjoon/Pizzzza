132027c8929ea965ee95340b140a4a68e2029e0a
한준 <s22073@gsm.hs.kr>
2023-05-02 12:27:23 +0900

    📢:: 라우터 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Main from './pages/Main';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Routes } from 'react-router';
+
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Main />}/>
+        <Route path='/domino' element={<Main />}/>
+        <Route path='/papajohns' element={<Main />}/>
+        <Route path='/mrpizza' element={<Main />}/>
+      </Routes>
+    </Router>
   );
 }
 
