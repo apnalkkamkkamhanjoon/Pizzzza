96eff8415321daf3a3ebcf8f438758b88b4451dd
한준 <s22073@gsm.hs.kr>
2023-05-03 13:40:27 +0900

    📢:: pizzaschool 부활 및 import path 변경

---
+++ src/pages/Pizzaschool.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import Pizzacontainer from '../components/Pizzacontainer'
+import '../styles/main.scss'
+
+const Domino = () => {
+    return (
+        <div className='mainContainer'>
+            <NavBar />
+        </div>
+    );
+};
+
+export default Domino;
