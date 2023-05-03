9fe19b514442354a5d98a2a559ac55ed1f94e999
한준 <s22073@gsm.hs.kr>
2023-05-03 13:44:42 +0900

    📢:: pizzacontainer 로직 수정 및 디자인 초안

--- src/pages/Papajohns.tsx
+++ src/pages/Papajohns.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import NavBar from '../components/NavBar';
 import '../styles/main.scss'
+import PaPaJohns from '../json/papajohns.json';
+import Pizzacontainer from '../components/Pizzacontainer'
+
+interface Pizza {
+    이미지: string;
+    품명: string;
+}
+
 
 const Papajohns = () => {
+    const [papajohns, setPapajohns] = useState<Pizza[]>([]);
+
+    useEffect(() => {
+        setPapajohns(PaPaJohns)
+    }, [])
+
+    const randomIndex = Math.floor(Math.random() * papajohns.length);
+
+
     return (
         <div className='mainContainer'>
             <NavBar />
+            <Pizzacontainer title={'파파존스'} imgSrc={papajohns[randomIndex]?.이미지} name={papajohns[randomIndex]?.품명} />
         </div>
     );
 };
