636003f67e4e9d8092daa39a137bb7fa9e40d0f5
한준 <s22073@gsm.hs.kr>
2023-05-03 14:12:19 +0900

    📢:: 새로고침 버튼 로직 구현

--- src/components/Reload.tsx
+++ src/components/Reload.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { GrPowerReset } from "react-icons/gr";
 import '../styles/reload.scss'
 
+const handleClick = () =>{
+    window.location.reload();
+}
+
 const Reload = () => {
     return (
         <div>
-            <GrPowerReset className='reload'/>
+            <GrPowerReset className='reload' onClick={handleClick}/>
         </div>
     );
 };
