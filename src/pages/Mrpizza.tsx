33236c2a3c570aed560bd613a49e5050924233b6
한준 <s22073@gsm.hs.kr>
2023-05-04 23:42:30 +0900

    📢:: Mrpizza 로직 삭제

--- src/pages/Mrpizza.tsx
+++ src/pages/Mrpizza.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import '../styles/main.scss'
+import PizzaContainer from '../components/Pizzacontainer';
 
 const Mrpizza = () => {
     return (
         <div className='mainContainer'>
             <NavBar />
+            <h1> 아직 구현 중입니다!</h1>
         </div>
     );
 };
